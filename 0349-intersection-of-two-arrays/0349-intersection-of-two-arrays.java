39e52dd17a2f1c2e88dcbd620075f9bdc55442ab
Daksh Saini <dakshhh98@gmail.com>
2026-01-21 23:17:18 +0530

    Time: 3 ms (55.29%), Space: 44.9 MB (74.13%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -7,9 +7,9 @@ class Solution {
         for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
 
+                    boolean exist = false;
                 if (nums1[i] == nums2[j]) {
 
-                    boolean exist = false;
 
                     // ONLY check, do not add here
                     for (int z = 0; z < k; z++) {
