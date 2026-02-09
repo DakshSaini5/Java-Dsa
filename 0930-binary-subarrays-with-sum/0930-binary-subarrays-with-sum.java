76eafaf6f20387ba27ace38e68e5d89d47f43de6
Daksh Saini <dakshhh98@gmail.com>
2026-02-09 18:54:21 +0530

    Time: 22 ms (40.82%), Space: 52.4 MB (25.09%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -6,8 +6,8 @@ class Solution {
         int sum = 0;
         int count = 0;
 
-        for(int i = 0; i < nums.length; i++){
-            sum = sum + nums[i];
+        for(int i : nums){
+            sum += i;
 
             if(map.containsKey(sum - goal)){
                 count += map.get(sum - goal);
