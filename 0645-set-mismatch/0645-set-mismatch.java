97ef7cb3cecea9102ac51d2bffc18c6f83ad74b3
Daksh Saini <dakshhh98@gmail.com>
2026-03-20 21:36:05 +0530

    Time: 3 ms (65.04%), Space: 47.3 MB (92.61%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,32 +1,48 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
      HashMap <Integer,Integer> map = new HashMap<>();
-    //  List <Integer> list = new List<>();
+    int n = nums.length;
+    int i = 0;
+
+    while(i < n){
+        int CrIndex = nums[i] - 1;
+
+        if(nums[i] != nums[CrIndex]){
+            int temp = nums[i];
+            nums[i] = nums[CrIndex];
+            nums[CrIndex] = temp;
+        }
+        else { 
+            i++;
+        }
+    }
+
+    for(i = 0; i < n; i++){
+        if(nums[i] != i+1){
+            return new int[]{nums[i], i+1};
+        }
+    }
+
+        return new int[]{-1,-1};
+        }
+    }    
+    //  List <Integer> list = new List<>(); {USED IN BRUTE FORCE}
 
     // int count; 
-    int duplicate = -1;
-    int missing = -1;
+    //  BETTER APPROACH int duplicate = -1;
+    // int missing = -1;
 
-     for(int num : nums){
-        map.put(num, map.getOrDefault(num,0)+1);
-     }
+    //  for(int num : nums){
+    //     map.put(num, map.getOrDefault(num,0)+1);
+    //  }
 
-     for(int i = 0; i <= nums.length; i++){
-        if(map.getOrDefault(i,0) == 2){
-            duplicate = i;
-        }
-        if(map.getOrDefault(i,0) == 0){
-            missing = i;
-        }
-      }
-      return new int[]{duplicate,missing};
-      }
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter w = new FileWriter("display_runtime.txt")) {
-                w.write("-0");
-            } catch (Exception e) {
-            }
-        }));
-    }
-}
+    //  for(int i = 0; i <= nums.length; i++){
+    //     if(map.getOrDefault(i,0) == 2){
+    //         duplicate = i;
+    //     }
+    //     if(map.getOrDefault(i,0) == 0){
+    //         missing = i;
+    //     }
+    //   }
+    //   return new int[]{duplicate,missing};
+    //   }
