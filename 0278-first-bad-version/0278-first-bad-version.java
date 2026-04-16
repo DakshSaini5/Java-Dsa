296123dcdcff644ecd5db932aee3cd566c0e89d1
Daksh Saini <dakshhh98@gmail.com>
2026-04-16 23:18:04 +0530

    Time: 13 ms (99.95%), Space: 42.3 MB (22.69%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,21 +3,21 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int start = 0;
+        int st = 0;
         int end = n;
 
         if(n == 1) return 1;
 
-        while(start < end){
-            int mid = start + (end-start)/2;
+        while(st < end){
+            int mid = st + (end-st)/2;
 
             if(isBadVersion(mid)){
                 end = mid;
             }
             else{
-                start = mid + 1;
+                st = mid + 1;
             }
         }
-        return start;
+        return st;
     }
 }
