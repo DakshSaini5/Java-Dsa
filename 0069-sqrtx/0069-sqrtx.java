70f1e9da30bc95eb97d12d861e5e0ba53a70c19c
Daksh Saini <dakshhh98@gmail.com>
2026-04-20 22:37:30 +0530

    Time: 1 ms (97.82%), Space: 42.5 MB (80.54%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -4,18 +4,18 @@ class Solution {
 
         int low = 1;
         int high = x;
-        int ans = 0;
+        int answer = 0;
         while(low <= high){
             int mid = low + (high - low)/2;
 
             if((long) mid * mid <= x){
-                ans = mid;
+                answer = mid;
                 low = mid + 1;
             }
             else{
                 high = mid - 1;
             }
         }
-        return ans;
+        return answer;
     }
 }
