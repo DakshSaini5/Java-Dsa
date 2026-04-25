f64767e33b60dedb65a2dc7f011cc76e945e4878
Daksh Saini <dakshhh98@gmail.com>
2026-04-25 18:37:52 +0530

    Time: 0 ms (100%), Space: 43 MB (71.29%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,36 +1,39 @@
 class Solution {
-    public int splitArray(int[] nums, int k) {
-        
-        int start = 0, end = 0;
+public int splitArray(int[] nums, int k) {
+    int start = 0, end = 0;
 
-        for (int num : nums) {
-            start = Math.max(start, num); 
-            end += num;              
-        }
-
-      while (start < end) {
-
-            int mid = start + (end - start) / 2;
-
-            int sum = 0;
-            int count = 1;
-
-            for (int num : nums) {
-                if (sum + num > mid) {
-                    count++;
-                    sum = num;
-                } else {
-                    sum += num;
-                }
-            }
-
-            if (count > k) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-
-        return start;
+    for (int num : nums) {
+        start = Math.max(start, num); 
+        end += num;              
     }
+
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+
+        if (canSplit(nums, mid, k)) {
+            end = mid;        // valid → try smaller
+        } else {
+            start = mid + 1;  // invalid → increase
+        }
+    }
+
+    return start;
+}
+
+private boolean canSplit(int[] nums, int mid, int k) {
+    int sum = 0;
+    int count = 1;
+
+    for (int num : nums) {
+        if (sum + num > mid) {
+            count++;
+            sum = num;
+        } else {
+            sum += num;
+        }
+    }
+
+    return count <= k;
+}
+
 }
