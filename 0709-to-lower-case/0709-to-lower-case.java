7b27165869d7e8f9fc19fc0ee22a2f53f5a201c0
Daksh Saini <dakshhh98@gmail.com>
2026-05-21 23:38:38 +0530

    Time: 0 ms (100%), Space: 42.8 MB (81.53%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,8 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        String lower = s.toLowerCase();
-        System.out.println(lower);
-        return lower;
-    }
-
+        return s.toLowerCase();
+        }
 }
