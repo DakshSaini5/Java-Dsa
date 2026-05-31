170acb84de639ff0b7d5b89c204aff4e66727d15
Daksh Saini <dakshhh98@gmail.com>
2026-05-31 23:58:40 +0530

    Time: 4 ms (58.03%), Space: 46.7 MB (37.94%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,6 +1,7 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        // 1. Set up our lookup table
+        HashMap<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
@@ -8,18 +9,21 @@ class Solution {
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
+        
+        // 2. Initialize our tracking variables
+        int total = 0;
+        int last = 0;
 
-        int sum = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            int curr = map.get(s.charAt(i));
-
-            if (i + 1 < s.length() && curr < map.get(s.charAt(i + 1))) {
-                sum -= curr;
-            } else {
-                sum += curr;
+        for(int i = s.length() - 1; i >= 0; i--){
+            int currentValue = map.get(s.charAt(i));
+            if(currentValue >= last){
+                total = total + currentValue;
             }
-        }
-        return sum;
+            else{
+                total = total - currentValue;
+            }
+            last = currentValue;
+        }       
+        return total; 
     }
 }
